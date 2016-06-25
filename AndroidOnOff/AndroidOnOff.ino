824626da9ce751a9fa19198da300f82d6cf60642
M. Dietrich <mdt@emdete.de>
2016-06-25 15:42:37 +0200

    add temp

--- AndroidOnOff/AndroidOnOff.ino
+++ AndroidOnOff/AndroidOnOff.ino
@@ -1,48 +1,68 @@
 #include <SoftwareSerial.h>
+#include <TimerOne.h>
 
-SoftwareSerial BT(10, 11); 
+SoftwareSerial BT(10, 11);
 // creates a "virtual" serial port/UART
 // connect BT module TX to D10
 // connect BT module RX to D11
 // connect BT Vcc to 5V, GND to GND
 
-void setup()  
+int LM35 = A0; // line where LM35 is connected
+float tempC; // temperature in celsius
+int reading; // value read from ADC
+char a; // stores incoming character from other device
+
+void setup()
 {
+  // set 1.1v reference
+  analogReference(INTERNAL);
+  // timer
+  Timer1.initialize(100);
+  Timer1.attachInterrupt(wakeUp);
   // set digital pin to control as an output
   pinMode(13, OUTPUT);
-
   // set the data rate for the SoftwareSerial port
   BT.begin(9600);
-
   // Send test message to other device
   BT.println("Hello from Arduino");
 }
 
-char a; // stores incoming character from other device
+void wakeUp() {
+  BT.println(tempC);
+}
 
-void loop() 
+void loop()
 {
-  if (BT.available())
-    // if text arrived in from BT serial...
+  if (BT.available()) // if text arrived in from BT serial...
   {
     a=(BT.read());
     if (a=='1')
     {
       digitalWrite(13, HIGH);
       BT.println("LED on");
+      BT.println(tempC);
     }
-    if (a=='2')
+    else if (a=='0')
     {
       digitalWrite(13, LOW);
       BT.println("LED off");
+      BT.println(tempC);
     }
-    if (a=='?')
+    else if (a=='?')
     {
       BT.println("Send '1' to turn LED on");
-      BT.println("Send '2' to turn LED on");
-    }   
-    // you can add more "if" statements with other characters to add more commands
+      BT.println("Send '0' to turn LED on");
+      BT.println(tempC);
+    }
+    else {
+      BT.println(tempC);
+    }
+  }
+  else
+  {
+    delay(100);
+    int i;
+    reading = analogRead(LM35);
+    tempC = reading / 9.31;
   }
 }
-
-
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 HC06
 ====
 
-Demonstration code for our HC06 Bluetooth module. Visit the product page at the
+Demonstration code for oBluetooth module. Visit the product page at the
 following URL:
 http://tronixlabs.com/breakout-boards/bluetooth/bluetooth-to-uart-serial-wireless-adaptor/
 for product information, tutorial links and more.
 
 Connection
-==========
+----------
 
 Arduino <---> Bluetooth Module
 
@@ -18,8 +18,27 @@ GND to GND
 D10 to TX
 D11 to RX
 
-Links
-=====
+Pairing
+-------
 
-http://www.instructables.com/id/Tutorial-Using-HC06-Bluetooth-to-Serial-Wireless-U/
+Name HC-06
+
+PIN 1234
+
+Links
+-----
+
+[HC06](http://www.instructables.com/id/Tutorial-Using-HC06-Bluetooth-to-Serial-Wireless-U/)
+
+LM35
+====
+
+![cabeling](LM35_Temperatur_Steckplatine.jpg)
+
+Links
+-----
+
+[LM35HigherResolution](http://playground.arduino.cc/Main/LM35HigherResolution)
+
+[aref](http://tronixstuff.com/2013/12/12/arduino-tutorials-chapter-22-aref-pin/)
 

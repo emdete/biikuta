53d9f940932c3a6ca3837509368369a4a8bf9314
M. Dietrich <mdt@emdete.de>
2016-06-25 21:58:46 +0200

    simplify structure

--- BlueToothTemperature.ino
+++ BlueToothTemperature.ino
@@ -1,5 +1,5 @@
-#include <SoftwareSerial.h>
-#include <TimerOne.h>
+#include <SoftwareSerial.h> // https://www.arduino.cc/en/Reference/SoftwareSerial
+#include <TimerOne.h> // 
 
 SoftwareSerial BT(10, 11);
 // creates a "virtual" serial port/UART
@@ -11,24 +11,36 @@ int LM35 = A0; // line where LM35 is connected
 float tempC; // temperature in celsius
 int reading; // value read from ADC
 char a; // stores incoming character from other device
+int on;
 
 void setup()
 {
+  on = 1;
   // set 1.1v reference
   analogReference(INTERNAL);
   // timer
-  Timer1.initialize(100);
+  Timer1.initialize(2000000);
   Timer1.attachInterrupt(wakeUp);
   // set digital pin to control as an output
   pinMode(13, OUTPUT);
   // set the data rate for the SoftwareSerial port
   BT.begin(9600);
   // Send test message to other device
-  BT.println("Hello from Arduino");
+  BT.println("bt temp measure");
 }
 
 void wakeUp() {
-  BT.println(tempC);
+  BT.print("temp=");
+  BT.print(tempC, 5);
+  BT.print("°C");
+  BT.println();
+  if (on) {
+      digitalWrite(13, HIGH);
+  }
+  else {
+      digitalWrite(13, LOW);
+  }
+  on = !on;
 }
 
 void loop()
@@ -38,31 +50,28 @@ void loop()
     a=(BT.read());
     if (a=='1')
     {
-      digitalWrite(13, HIGH);
+      on = 1;
       BT.println("LED on");
-      BT.println(tempC);
     }
     else if (a=='0')
     {
-      digitalWrite(13, LOW);
+      on = 0;
       BT.println("LED off");
-      BT.println(tempC);
     }
     else if (a=='?')
     {
       BT.println("Send '1' to turn LED on");
       BT.println("Send '0' to turn LED on");
-      BT.println(tempC);
     }
     else {
-      BT.println(tempC);
+      wakeUp();
     }
   }
   else
   {
     delay(100);
-    int i;
     reading = analogRead(LM35);
     tempC = reading / 9.31;
   }
 }
+
